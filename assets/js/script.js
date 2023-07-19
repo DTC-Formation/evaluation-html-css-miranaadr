dc30a1db05da43ab7fdd2251d0a4244fbe51b9cf
unknown <USER@T7-PMDC-23.thirtyone.local>
2023-07-19 15:41:08 +0300

    Intégration menu burger

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    let sidenav = document.getElementById("mySidenav");
+  sidenav.classList.add("active");
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    let sidenav = document.getElementById("mySidenav");
+  sidenav.classList.remove("active");
+}
